bcea243840a4a85138559d0d7b31556cca893ba7
Noah Z <zeilmannnoah@gmail.com>
2019-03-17 18:36:47 -0500

    Next to last word question

---
+++ NextToLastWord.java
@@ -0,0 +1,20 @@
+/*
+    Get the next-to-the-last word in a sentence. 
+*/
+
+import java.util.Arrays;
+import java.util.List;
+
+class NextToLastWord {
+    public static String nextToLastWord(String sentence) {
+        List<String> words = Arrays.asList(sentence.split("\\s"));
+
+        return words.get(words.size() - 2); 
+    }
+
+    public static void main(String[] args) {
+        String sentence = "Hello, it would be pretty neat if I aced this interview with ease.";
+
+        System.out.println(nextToLastWord(sentence));
+    }
+}
