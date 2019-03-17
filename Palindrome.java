414c51fd7547fb5d896320384a62f4e1ef4a01c9
Noah Z <zeilmannnoah@gmail.com>
2019-03-17 18:37:31 -0500

    Palindrome question

---
+++ Palindrome.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.Integer;
+
+/*
+    A palindrome number is a number that is same after reverse. For example 545, 151, 34543, 343, 171, 48984
+    are the palindrome numbers. It can also be a string like LOL, MADAM etc.
+*/
+
+class Palindrome {
+    public static boolean isPalindrome(int num) {
+        String numString  = (new Integer(num)).toString();
+        
+        return isPalindrome(numString);
+    }
+
+    public static boolean isPalindrome(String word) {
+        List<String> numList = Arrays.asList(word.split(""));
+
+        Collections.reverse(numList);
+
+        return String.join("", numList).equalsIgnoreCase(word);
+    }
+
+    public static void main(String[] args) {
+        int num = 454;
+        int incorrectNum = 726;
+        String word = "Racecar";
+        String incorrectWord = "Noah";
+
+        System.out.printf("%d is %s\n", num, isPalindrome(num));
+        System.out.printf("%d is %s\n", incorrectNum, isPalindrome(incorrectNum));
+        System.out.printf("%s is %s\n", word, isPalindrome(word));
+        System.out.printf("%s is %s\n", incorrectWord, isPalindrome(incorrectWord));
+    }
+}
